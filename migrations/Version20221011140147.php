9218582fdd995ed39918198a1698e1a478d9c960
Incaf Ait Oumaksa <hdidana.incaf@gmail.com>
2022-10-12 12:27:52 +0200

    Add img tricks

---
+++ migrations/Version20221011140147.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221011140147 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, tricks_id INT DEFAULT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_9474526CA76ED395 (user_id), INDEX IDX_9474526C3B153154 (tricks_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, tricks_id INT DEFAULT NULL, path_img VARCHAR(255) NOT NULL, INDEX IDX_C53D045F3B153154 (tricks_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tricks (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(200) NOT NULL, description LONGTEXT NOT NULL, catégory VARCHAR(100) NOT NULL, slug VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_E1D902C1A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, user_name VARCHAR(50) NOT NULL, avatar VARCHAR(255) DEFAULT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, tricks_id INT DEFAULT NULL, url_video VARCHAR(255) NOT NULL, INDEX IDX_7CC7DA2C3B153154 (tricks_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C3B153154 FOREIGN KEY (tricks_id) REFERENCES tricks (id)');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045F3B153154 FOREIGN KEY (tricks_id) REFERENCES tricks (id)');
+        $this->addSql('ALTER TABLE tricks ADD CONSTRAINT FK_E1D902C1A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2C3B153154 FOREIGN KEY (tricks_id) REFERENCES tricks (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CA76ED395');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C3B153154');
+        $this->addSql('ALTER TABLE image DROP FOREIGN KEY FK_C53D045F3B153154');
+        $this->addSql('ALTER TABLE tricks DROP FOREIGN KEY FK_E1D902C1A76ED395');
+        $this->addSql('ALTER TABLE video DROP FOREIGN KEY FK_7CC7DA2C3B153154');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE tricks');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE video');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,7 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
+use App\Entity\Image;
+use App\Entity\Tricks;
 use App\Entity\User;
+use App\Entity\Video;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -28,34 +32,91 @@ class UserFixtures extends Fixture
         // $manager->persist($product);
 
         $faker =Factory::create('fr_FR');
+        $users = [];
+        $categories = [];
         $genders = ['male', 'female'];
         $gender = $faker->randomElement($genders);
         $date  = new \DateTimeImmutable();
+        $category = ['Grabs', 'Rotations', 'Flips', 'Rotations désaxées', 'Slides', 'One foot', 'Old school'];
+        $tricksName = ['Mute', 'Indy', '360', '720', 'Backflip', 'Misty', 'Tail slide', 'Method air', 'Backside air'];
+
+        //Admin
         $admin = new User();
         $admin->setUserName('Incaf Ait Oumaksa');
         $admin->setEmail('admin@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
         $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
-        $admin->setIsAdmin(1);
         $admin->setCreatedAt($date);
         $admin->setUpdatedAt($date);
         $manager->persist($admin);
-        
-        for ($i=1; $i<=3;$i++) {
+
+        //User
+        for ($i=1; $i<=3;$i++) 
+        {
             $user = new User();
             
             $user->setUserName($faker->name);
             $user->setEmail("user$i@gmail.com");
             $user->setPassword($this->passwordHasher->hashPassword($user, 'admin'));
             $user->setAvatar('https://randomuser.me/api/portraits/' . ($gender == 'male' ? 'men/' : 'women/') . $faker->numberBetween(1,99) . '.jpg');
-            $user->setIsAdmin(0);
             $user->setCreatedAt($date);
             $user->setUpdatedAt($date);
             $manager->persist($user);
+            $users[] = $user;
         }
+
+        //Génerate tricks 
+        foreach ($category as $categoryName)
+        {
+            var_dump($categoryName);
+            $categories[] = $categoryName;
+        }
+         
+
+        foreach ($tricksName as $trickName)
+        {
+            $trick = new Tricks();
+            $trick->setName($trickName);
+            $trick->setDescription($faker->paragraph(5));
+            $trick->setCreatedAt($date);
+            $trick->setUpdatedAt($date);
+            $trick->setSlug($trickName);
+            $trick->setCatégory($faker->randomElement($categories));
+            $trick->setUser($faker->randomElement($users));
+            for ($k=1; $k<4; $k++)
+            {
+                $image = new Image();
+                $image->setPathImg('img/tricks' . $trick->getName() . ' ' . $k . '.jpg');
+                $image->setTricks($trick);
+                $manager->persist($image);
+
+            }
+
+            // 1 to 2 Video by Trick
+            for ($l=0; $l<mt_rand(1, 2); $l++)
+            {
+            $video = new Video();
+            $video->setUrlVideo('https://www.youtube.com/watch?v=tHHxTHZwFUw');
+            $video->setTricks($trick);      
+            $manager->persist($video);
+            }
+            // 0 to 30 Comment by Trick
+            for ($m=0; $m<mt_rand(0, 30); $m++)
+            {
+                $comment = new Comment();
+                $comment->setContent($faker->sentence(mt_rand(1, 5)));
+                $comment->setCreatedAt($date);
+                $comment->setUpdatedAt($date);
+                $comment->setUser($faker->randomElement($users));
+                $comment->setTricks($trick);
+                
+                $manager->persist($comment);
+            } 
+
+        }
+
+        
         $manager->flush();
     }
-    public static function getGroups(): array
-    {
-        return ['user'];
-    }
+ 
 }
