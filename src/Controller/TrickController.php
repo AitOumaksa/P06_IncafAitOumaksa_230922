bff2e23cd0f01794a0fee735ced3557400ad9595
Incaf Ait Oumaksa <hdidana.incaf@gmail.com>
2022-10-24 11:22:47 +0200

    create template twig

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -7,9 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TrickRepository;
 
+#[Route('/trick')]
 class TrickController extends AbstractController
 {
-    #[Route('/trick/{slug}', name: 'trick_details')]
+    #[Route('/{slug}', name: 'trick_details')]
     public function getTrick(TrickRepository $repo, $slug): Response
     {
         $trick = $repo->findOneBySlug($slug);
