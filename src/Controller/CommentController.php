684e61dd2304d0a4c70d1c7d11f2b6f3c64aaa53
Incaf Ait Oumaksa <hdidana.incaf@gmail.com>
2022-11-01 18:52:14 +0100

    Comment edit

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,14 +2,53 @@
 
 namespace App\Controller;
 
+
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route("trick")]
 class CommentController extends AbstractController
 {
-    #[Route('/trick/delete/comment/{id}', name: 'delete.comment')]
+
+    
+    #[Route("/edit/comment/{id}/", name : "edit.comment")]
+   
+    public function edit(CommentRepository $repo, Request $request , $id ,ManagerRegistry $doctrine)
+    {
+       
+        $comment = $repo->findOneBy(['id' => $id]);
+
+        $manager = $doctrine->getManager();
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $comment->setUpdatedAt(new \DateTimeImmutable());
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre commentaire a bien été modifié !'
+            );
+
+            return $this->redirectToRoute('trick.details', array('slug' => $comment->getTrick()->getSlug()));
+        }
+
+        return $this->render('comment/edit-comment.html.twig', [
+            'comment' => $comment,
+            'form' => $form,
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/delete/comment/{id}', name: 'delete.comment')]
     public function deleteComment(CommentRepository $repo, ManagerRegistry $doctrine, $id)
     {  
 
