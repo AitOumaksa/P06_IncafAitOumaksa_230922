ee6689ced1935b8e8c2aab97a7682c35ddc94c96
Incaf Ait Oumaksa <hdidana.incaf@gmail.com>
2022-11-14 22:16:20 +0100

    LoadMoreTricks pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,9 +12,20 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(TrickRepository $repo): Response
     {
-        $tricks = $repo->findBy([], ['updatedAt' => 'DESC']);
+        $tricks = $repo->findBy([], ['updatedAt' => 'DESC'] , 10, 0);
 
         return $this->render('home/index.html.twig', [
+            'tricks' => $tricks
+        ]);
+    }
+    
+    #[Route('/{start}', name:'load.more', requirements:['start' => '\d+'])]
+    public function loadMoreTricks(TrickRepository $repo, $start = 10)
+    {
+        // Get 15 tricks from the start position
+        $tricks = $repo->findBy([], ['createdAt' => 'DESC'], 10, $start);
+
+        return $this->render('home/load-more.html.twig', [
             'tricks' => $tricks
         ]);
     }
